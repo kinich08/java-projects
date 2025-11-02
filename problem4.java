61d2592a3385954db1b07470030220133a03d43a
kinich08 <antorbhowmik44588@gmail.com>
2025-11-02 09:14:53 +0600

    .....

---
+++ problem4.java
@@ -0,0 +1,60 @@
+
+abstract class Animal {
+    public abstract void eat();
+
+    public abstract void sleep();
+}
+class Lion extends Animal {
+    @Override
+    public void eat() {
+    
+        System.out.println("\nLion eats meat.");
+    }
+    @Override
+    public void sleep() {
+        // Print where the lion sleeps
+        System.out.println("Lion sleeps on grassland.");
+    }
+}
+
+class Tiger extends Animal {
+    @Override
+    public void eat() {
+        System.out.println("\n\nTiger eats meat and occasionally hunts in water.");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println("Tiger sleeps in a hidden spot.");
+    }
+} 
+class Deer extends Animal {
+    @Override
+    public void eat() {
+        System.out.println("\n\nDeer grazes on grass and leaves.");
+    }
+    @Override
+    public void sleep() {
+        System.out.println("Deer sleeps in open fields.\n");
+    }
+}
+
+public class problem4 {
+    public static void main(String[] args) {
+        Lion lion = new Lion();
+        lion.eat();
+        lion.sleep();
+
+        //System.out.println();
+
+        Tiger tiger = new Tiger();
+        tiger.eat();
+        tiger.sleep();
+
+        //System.out.println();
+        Deer deer = new Deer();
+        deer.eat();
+        deer.sleep();
+    }
+} 
+
